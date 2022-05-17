44b9958cc01a07bdcf20eac8afff6621f100b722
mohamad.al <mohamadali.bc@gmail.com>
2022-05-17 03:26:52 +0200

    homePageStudent

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import RegStudentPage from "./components/RegStudentPage/RegStudentPage";
 import RegFirmaPage from "./components/RegFirmaPage/RegFirmaPage";
 import AddOfferPage from "./components/AddOfferPage/AddOfferPage";
+import HomePageStudent from "./components/HomePageStudent/HomePageStudent";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <AddOfferPage />;
+  return <HomePageStudent />;
 }
 
 export default App;
