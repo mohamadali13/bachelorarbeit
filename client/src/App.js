57c6aff4eda853e969ddd27d540af6926919221d
mohamad.al <mohamadali.bc@gmail.com>
2022-05-21 22:53:18 +0200

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,27 @@ import AddOfferPage from "./components/AddOfferPage/AddOfferPage";
 import HomePageStudent from "./components/HomePageStudent/HomePageStudent";
 import HomePageAdmin from "./components/HomePageAdmin/HomePageAdmin";
 import HomePageFrima from "./components/HomePageFirma/HomePageFirma";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AppliedSuscess from "./components/AppliedSucsessPage/AppliedSucsessPage";
 import JobStatusForStudent from "./components/JobStatusForStudent/JobStatusForStudent";
 import JobStatusForAdmin from "./components/JobStatusForAdmin/JobStatusForAdmin";
+
 function App() {
-  return <JobStatusForAdmin />;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="loginPage" element={<LoginPage />} />
+      <Route path="regStudentPage" element={<RegStudentPage />} />
+      <Route path="AddOfferPage" element={<AddOfferPage />} />
+      <Route path="regFirmaPage" element={<RegFirmaPage />} />
+      <Route path="homePageStudent" element={<HomePageStudent />} />
+      <Route path="homePageAdmin" element={<HomePageAdmin/>} />
+      <Route path="homePageFrima" element={<HomePageFrima />} />
+      <Route path="appliedSuscess" element={<AppliedSuscess />} />
+      <Route path="jobStatusForStudent" element={<JobStatusForStudent />} />
+      <Route path="jobStatusForAdmin" element={<JobStatusForAdmin />} />
+    </Routes>
+  );
 }
 
 export default App;
